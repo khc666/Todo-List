87a7f709301f8a4191e81961269d5441522c24f2
zhousir <2919150@qq.com>
2019-03-20 14:31:20 +0800

    表单添加列表

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       list: ['html','css','js','vue'],
+       inputValue: ''
+    }
+    this.onHandleChange = this.onHandleChange.bind(this);
+    this.onHandleClick = this.onHandleClick.bind(this);
+  }
+
+  onHandleChange(e){
+      this.setState({
+        inputValue: e.target.value
+      });
+  }
+
+  onHandleClick(){
+      let list = this.state.list;
+      list.push(this.state.inputValue);
+
+      this.setState({
+        list,
+        inputValue: ''
+      });
+  }
+
   render() {
     return (
       <div>
-         hello world
+          <input  type='text' value={this.state.inputValue} onChange={this.onHandleChange}/><button onClick={this.onHandleClick}>添加</button>
+          <ul>
+            {
+              this.state.list.map( (item,index) =>{
+                return <li key={index}>{item}</li>
+              } )
+            }
+          </ul>
       </div>
     );
   }
